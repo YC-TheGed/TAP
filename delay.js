c202080e8df9f5c754fc5c5108ac42d51a602dcf
YC-TheGed <167610411+YC-TheGed@users.noreply.github.com>
2024-06-11 11:12:53 +0800

    Update delay.js

--- delay.js
+++ delay.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Set a timeout to show the container after 3 seconds
   setTimeout(function () {
     document.getElementById("hide").style.display = "block";
-  }, 5000); // 3000 milliseconds = 3 seconds
+  }, 1310000); 
 });
